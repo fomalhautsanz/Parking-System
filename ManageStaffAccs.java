5b53ecc4e145edec62f34f360333be4d977c7d7e
Fomalhaut Sanz <saladatrixy@gmail.com>
2024-12-07 18:02:32 +0800

    Update ManageStaffAccs.java

--- ManageStaffAccs.java
+++ ManageStaffAccs.java
@@ -1,54 +1,78 @@
+
+import java.time.format.*;
+import java.util.*;
+import java.time.LocalDateTime;
 import java.util.Scanner;
 
-public class ManageStaffAccs {
+public class Scratch {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        //SCANNER FOR MENU
         int managestaffmenu;
-        //CREATE A NEW STAFF ACCOUNT
-        String[] staffdetails  = {"Enter new staff username: ", "Enter new staff password: "};
-        String[][] staffdetailsinput=new String[5][3];
-        int staffcont = 0;
-        //DISPLAY STAFF
-        String[] display = {"#","Username","Status","Last Log In"};
+        //1. CREATE NEW STAFF ACC
+        String[][] staffs = new String[5][4];
+        int count=0;
+        DateTimeFormatter formatter
+                = DateTimeFormatter.ofPattern(
+                "yyyy-MM-dd HH:mm:ss a");
 
-        do {
+        while (true){
             System.out.println("===================================");
             System.out.println("MANAGE STAFF ACCOUNTS");
             System.out.println("===================================");
             System.out.println("1. Create New Staff Account");
             System.out.println("2. View All Staff");
             System.out.println("3. Back to Dashboard");
+            System.out.print("Select an option: ");
+
             managestaffmenu = input.nextInt();
+            input.nextLine();
 
-            if (managestaffmenu == 1) {
+            switch (managestaffmenu){
+                case 1:
+                if (count>=staffs.length){
+                    System.out.println("Limit reached. Cannot create more accounts. Please contact the system developers.");
+                }
+                else {
+                        System.out.print("Enter new staff username: ");
+                        staffs[count][0] = input.nextLine();
+                        System.out.print("Enter new staff password: ");
+                        staffs[count][1] = input.nextLine();
+                        staffs[count][2] = "Active";
+                        LocalDateTime loginDateTime  = LocalDateTime.now();
+                        String convertedloginDateTime = loginDateTime.format(formatter);
+                        staffs[count][3] = convertedloginDateTime;
+                        count++;
+                }
+                break;
 
-                do {
-                    for (int i = 0; i < 5; i++) {
-                        input.nextLine();
-                        for (int j = 0; j < 2; j++) {
-                            System.out.print(staffdetails[j] + " ");
-                            staffdetailsinput[i][j] = input.nextLine();
-                        }
-                        System.out.println("Staff account created successfully");
-                        System.out.print("Do you want to create another account? [1] Yes [2] No ");
-                        staffcont=input.nextInt();
-                        if (staffcont==2){
-                            break;
-                        }
-                    }
-                }while (staffcont!=2);
+                case 2:
+                 displayStaffs(staffs,count);
+                 break;
             }
-            else if (managestaffmenu == 2) {
-                
 
-
-            }
-            else if (managestaffmenu == 3) {
-
-            }
-            else {
-
-            }
-        } while (true);
+        }
     }
+
+    public static void displayStaffs(String[][] staffs, int count){
+        String[] headers = {"#", "Username", "Status", "Last Login"};
+
+        for (String header:headers){
+            System.out.print(header + "\t");
+        }
+
+        System.out.println();
+        for (int i=0; i<count; i++){
+            System.out.print((i+1) + "\t");
+            for (int j=0; j<staffs[i].length; j++){
+            if (j==1){
+                continue;
+            }
+                System.out.print(staffs[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
 }
+
